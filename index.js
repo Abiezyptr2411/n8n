b4406b3f605c56177a71a99c6cc1bfa445ed46c2
Abiezy Syahdilla Putera <syahdillaabiezy@gmail.com>
2026-06-04 10:56:26 +0700

    refactor: restrict dotenv configuration to non-production environments

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const midtransClient = require('midtrans-client');
 const axios = require('axios');
 const path = require('path');
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 
 const app = express();
 app.use(express.json());
